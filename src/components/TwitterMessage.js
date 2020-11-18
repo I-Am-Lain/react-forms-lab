ace4db47705b30020b61856719331592d8369dc0
I-Am-Lain <vetter.william@gmail.com>
2020-11-18 16:39:45 -0600

    Done.

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,42 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {
+      value: '',
+      limit: props.maxChars
+    };
+  }
+
+  handleChange = (event) => {
+    const e = event
+
+
+    this.setState({
+      value: e.target.value,
+      limit: this.state.limit - 1
+    })
+
+
+    // if (e.nativeEvent.inputType === 'deleteContentBackward'){
+    //   this.setState({
+    //     limit: this.state.limit +1
+    //   })
+    // } else {
+    //   this.setState({
+    //     limit: this.state.limit -1
+    //   })
+    // }
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" value={this.state.value} onChange={this.handleChange}/>
+        <p>{this.state.limit}</p>
       </div>
     );
   }
